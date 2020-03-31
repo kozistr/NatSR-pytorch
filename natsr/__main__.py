0c77f754eaa10adc009e62d6f195fc2bacab37cf
kozistr <kozistr@gmail.com>
2020-03-31 16:08:09 +0900

    feat: caller for train(), test()

--- natsr/__main__.py
+++ natsr/__main__.py
@@ -1,10 +1,19 @@
 from natsr import CONFIG_FILENAME
+from natsr.test import test
+from natsr.train import train
 from natsr.utils import get_config
 
 
 def main():
     config = get_config(CONFIG_FILENAME)
-    print(config)
+
+    mode: str = config['mode']
+    if mode == 'train':
+        train(config)
+    elif mode == 'test':
+        test(config)
+    else:
+        raise NotImplementedError(f'[-] not supported mode : {mode}')
 
 
 main()
