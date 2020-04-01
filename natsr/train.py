0e286d4a0c3327b419f54889d02f0b814602264c
kozistr <kozistr@gmail.com>
2020-04-01 14:32:00 +0900

    update: baseline train.py - WIP

--- natsr/train.py
+++ natsr/train.py
@@ -1,2 +1,7 @@
+from natsr import DataType
+from natsr.dataloader import build_data_loader
+
+
 def train(config):
-    pass
+    train_data_loader = build_data_loader(config, data_type=DataType.TRAIN)
+    valid_data_loader = build_data_loader(config, data_type=DataType.VALID)
