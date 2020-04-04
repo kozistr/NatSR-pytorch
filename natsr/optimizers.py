fcb48f70aa3b20b9dd25e14fde0501378120b9e6
kozistr <kozistr@gmail.com>
2020-04-04 22:27:48 +0900

    refactor: the codes

--- natsr/optimizers.py
+++ natsr/optimizers.py
@@ -2,18 +2,29 @@ import torch.nn as nn
 from torch.optim import SGD, Adam
 
 from natsr import OptimizerType
+from natsr.utils import is_valid_key
 
 
 def build_optimizers(config, model: nn.Module):
     optimizer_type: str = config['model']['optimizer']
+    model_type: str = config['model']['model_type']
+
+    if not is_valid_key(config['model'], model_type):
+        raise NotImplementedError(
+            f'[-] not supported model_type : {model_type}'
+        )
 
     if optimizer_type == OptimizerType.ADAM:
         optimizer = Adam(
-            model.parameters(), lr=config['model']['lr'], amsgrad=False
+            model.parameters(),
+            lr=config['model'][model_type]['lr'],
+            amsgrad=False,
         )
     elif optimizer_type == OptimizerType.SGD:
         optimizer = SGD(
-            model.parameters(), lr=config['model']['lr'], nesterov=True
+            model.parameters(),
+            lr=config['model'][model_type]['lr'],
+            nesterov=True,
         )
     else:
         raise NotImplementedError(
--- natsr/schedulers.py
+++ natsr/schedulers.py
@@ -2,14 +2,23 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torch.optim.optimizer import Optimizer
 
 from natsr import LRSchedulerType
+from natsr.utils import is_valid_key
 
 
 def build_lr_scheduler(config, optimizer: Optimizer):
     lr_schedule_type: str = config['model']['lr_schedule_type']
+    model_type: str = config['model']['model_type']
+
+    if not is_valid_key(config['model'], model_type):
+        raise NotImplementedError(
+            f'[-] not supported model_type : {model_type}'
+        )
+
     if lr_schedule_type == LRSchedulerType.EXPONENTIAL:
         return ExponentialLR(
-            optimizer, gamma=config['model']['lr_decay_ratio']
+            optimizer, gamma=config['model'][model_type]['lr_decay_ratio']
         )
+
     raise NotImplementedError(
         f'[-] not supported lr_schedule_type {lr_schedule_type}'
     )
