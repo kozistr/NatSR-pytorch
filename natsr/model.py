d535861362ce47161f78b3a9a7e7e7938fb1804c
kozistr <kozistr@gmail.com>
2020-04-04 17:56:37 +0900

    feat: impl NMD

--- natsr/model.py
+++ natsr/model.py
@@ -148,11 +148,87 @@ class Fractal(nn.Module):
 
 
 class NMD(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
+
+        self.n_feats = self.config['model']['n_feats']
+
+        self.conv1_1 = nn.Conv2d(
+            self.config['model']['channel'],
+            self.n_feats * 1,
+            kernel_size=3,
+            padding=1,
+        )
+        self.conv1_2 = nn.Conv2d(
+            self.n_feats * 1, self.n_feats * 1, kernel_size=3, padding=1
+        )
+
+        self.conv2_1 = nn.Conv2d(
+            self.n_feats * 1, self.n_feats * 2, kernel_size=3, padding=1
+        )
+        self.conv2_2 = nn.Conv2d(
+            self.n_feats * 2, self.n_feats * 2, kernel_size=3, padding=1
+        )
+
+        self.conv3_1 = nn.Conv2d(
+            self.n_feats * 2, self.n_feats * 4, kernel_size=3, padding=1
+        )
+        self.conv3_2 = nn.Conv2d(
+            self.n_feats * 4, self.n_feats * 4, kernel_size=3, padding=1
+        )
+
+        self.conv4_1 = nn.Conv2d(
+            self.n_feats * 4, self.n_feats * 8, kernel_size=3, padding=1
+        )
+        self.conv4_2 = nn.Conv2d(
+            self.n_feats * 8, self.n_feats * 8, kernel_size=3, padding=1
+        )
+
+        self.conv5_1 = nn.Conv2d(self.n_feats * 8, 1, kernel_size=3, padding=0)
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.act = nn.ReLU()
+
+        self._weight_initialize()
+
+    def _weight_initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(
+                    m.weight, mode='fan_in', nonlinearity='relu'
+                )
 
     def forward(self, x):
-        pass
+        x = self.conv1_1(x)
+        x = self.act(x)
+        x = self.conv1_2(x)
+        x = self.act(x)
+        x = self.max_pool(x)
+
+        x = self.conv2_1(x)
+        x = self.act(x)
+        x = self.conv2_2(x)
+        x = self.act(x)
+        x = self.max_pool(x)
+
+        x = self.conv3_1(x)
+        x = self.act(x)
+        x = self.conv3_2(x)
+        x = self.act(x)
+        x = self.max_pool(x)
+
+        x = self.conv4_1(x)
+        x = self.act(x)
+        x = self.conv4_2(x)
+        x = self.act(x)
+        x = self.max_pool(x)
+
+        x = self.conv5_1(x)
+        x = self.gap(x)
+
+        return x
 
 
 class Discriminator(nn.Module):
