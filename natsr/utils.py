6708e52e6b44f9017cb9012b553a4bd7f8597a28
kozistr <kozistr@gmail.com>
2020-04-05 02:05:39 +0900

    feat: impl load_model & save_model

--- natsr/utils.py
+++ natsr/utils.py
@@ -1,6 +1,8 @@
+import os
 from typing import Dict
 
 import torch
+import torch.nn as nn
 import yaml
 
 
@@ -15,3 +17,26 @@ def is_valid_key(d: Dict[str, str], key: str) -> bool:
 
 def is_gpu_available() -> bool:
     return torch.cuda.is_available()
+
+
+def load_model(filepath: str, model: nn.Module, device: str):
+    epoch: int = 1
+
+    if os.path.exists(filepath):
+        checkpoint = torch.load(filepath, map_location=device)
+
+        try:
+            model.load_state_dict(checkpoint['model'])
+            epoch = checkpoint['epoch']
+        except KeyError:
+            model.load_state_dict(checkpoint)
+
+        print(f'[+] model {filepath} loaded! epoch : {epoch}')
+    else:
+        print(f'[-] model is not loaded :(')
+
+    return epoch
+
+
+def save_model(filepath: str, model: nn.Module, epoch: int):
+    torch.save({'model': model.state_dict(), 'epoch': epoch}, filepath)
