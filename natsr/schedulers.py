2e7e318b3ec9376d810e4863f2b65053de9cdf88
kozistr <kozistr@gmail.com>
2020-04-07 11:07:14 +0900

    update: TODO for lr scheduler

--- natsr/schedulers.py
+++ natsr/schedulers.py
@@ -4,12 +4,13 @@ from torch.optim.optimizer import Optimizer
 from natsr import LRSchedulerType
 
 
+# TODO : implement staircase exp lr scheduling
 def build_lr_scheduler(config, model_type: str, optimizer: Optimizer):
     lr_schedule_type: str = config['model'][model_type]['lr_schedule_type']
 
     if lr_schedule_type == LRSchedulerType.EXPONENTIAL:
         return ExponentialLR(
-            optimizer, gamma=config['model'][model_type]['lr_decay_ratio']
+            optimizer, gamma=config['model'][model_type]['lr_decay_ratio'],
         )
     raise NotImplementedError(
         f'[-] not supported lr_schedule_type {lr_schedule_type}'
