df77f34bc8faad56032f75cc22ca1247e0f19935
kozistr <kozistr@gmail.com>
2020-04-14 14:35:09 +0900

    feat: impl dct_8x8 (noise) & blur

--- natsr/utils.py
+++ natsr/utils.py
@@ -5,10 +5,12 @@ from typing import Dict, Optional, Tuple
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import yaml
 from tensorboardX import SummaryWriter
 
 from natsr import DeviceType, ModelType
+from scipy.fftpack import dct, idct
 
 
 def get_config(filename: str):
@@ -116,3 +118,36 @@ def log_summary(summary, data, global_step: int):
 
 def tensor_to_numpy(x: torch.Tensor) -> np.ndarray:
     return x.cpu().data.numpy()
+
+
+def inject_dct_8x8(x, sigma: float):
+    b, c, h, w = x.shape
+
+    x = np.reshape(x, [b, c, h // 8, 8, w // 8, 8])
+    x_dct = dct(x, axis=3, norm='ortho')
+    x_dct = dct(x_dct, axis=5, norm='ortho')
+
+    z = np.zeros([b, c, h // 8, 8, w // 8, 8])
+    noise_raw = np.random.randn(b, c, h // 8, 8, w // 8, 8) * sigma
+    z[:, :, :, 7, :, :] = noise_raw[:, :, :, 7, :, :]
+    z[:, :, :, :, :, 7] = noise_raw[:, :, :, :, :, 7]
+
+    x_dct_noise = x_dct + z
+
+    inv_x = idct(x_dct_noise, axis=3, norm='ortho')
+    inv_x = idct(inv_x, axis=5, norm='ortho')
+    inv_x = np.reshape(inv_x, x.shape)
+    return inv_x
+
+
+def get_noisy(img, sigma: float):
+    dct_img = inject_dct_8x8(tensor_to_numpy(img), sigma)
+    dct_img = torch.from_numpy(dct_img).float().to(img.device)
+    return dct_img
+
+
+def get_blurry(img, scale: int, alpha: float):
+    _img = F.interpolate(img, scale_factor=1.0 / scale, mode='bicubic')
+    _img = F.interpolate(_img, scale_factor=scale, mode='bicubic')
+    x_blurry = (1.0 - alpha) * _img + alpha * img
+    return x_blurry
